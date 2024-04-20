d229d3d68360fcb15cee72db4f41a5f408ac18fa
Weihan Li <weihanli@outlook.com>
2024-04-20 10:46:34 +0800

    Update window-defender-configure.ps1

--- window-defender-configure.ps1
+++ window-defender-configure.ps1
@@ -1,9 +1,18 @@
-Write-Host "Exclude folders from Windows Defender..." -ForegroundColor Green
-Add-MpPreference -ExclusionPath "$env:USERPROFILE\source\repos"
-Add-MpPreference -ExclusionPath "$env:USERPROFILE\.nuget"
-Add-MpPreference -ExclusionPath "$env:USERPROFILE\.vscode"
-Add-MpPreference -ExclusionPath "$env:USERPROFILE\.dotnet"
-Add-MpPreference -ExclusionPath "$env:USERPROFILE\.ssh"
-Add-MpPreference -ExclusionPath "$env:USERPROFILE\.kube"
-Add-MpPreference -ExclusionPath "$env:USERPROFILE\.ollama"
-Add-MpPreference -ExclusionPath "$env:APPDATA\npm"
+Write-Host "Add exclusions from Windows Defender..." -ForegroundColor Green
+
+$exclusions = (
+  "$env:USERPROFILE\source\repos"
+  "$env:USERPROFILE\.nuget"
+  "$env:USERPROFILE\.vscode"
+  "$env:USERPROFILE\.dotnet"
+  "$env:USERPROFILE\.ssh"
+  "$env:USERPROFILE\.kube"
+  "$env:USERPROFILE\.ollama"
+  "$env:APPDATA\npm"
+  "$env:SystemDrive\projects"
+  "$env:SystemDrive\documents"
+)
+
+foreach ($item in $exclusions) {
+  Add-MpPreference -ExclusionPath $item
+}
