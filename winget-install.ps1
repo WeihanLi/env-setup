59eb3f6c884bb918102b98ef3a8bfd7697e25eed
Weihan Li <weihanli@outlook.com>
2024-05-08 23:16:38 +0800

    Update winget-install.ps1

--- winget-install.ps1
+++ winget-install.ps1
@@ -55,24 +55,23 @@ Add-Content -Path $PROFILE -Value @psProfile
 Write-Host "PowerShell profile configured" -ForegroundColor Green
 Get-Content -Path $PROFILE
 
-Write-Host "Setting up Git for Windows..." -ForegroundColor Green
+Write-Host "Setting up Git ..." -ForegroundColor Green
 Write-Host "------------------------------------" -ForegroundColor Green
 # user config
-git config --global user.email "weihanli@outlook.com"
 git config --global user.name "Weihan Li"
+git config --global user.email "weihanli@outlook.com"
 # alias config
-git config --global alias.co checkout
+git config --global alias.co "checkout"
 git config --global alias.cob "checkout -b"
 git config --global alias.br "branch --format='%(HEAD) %(color:yellow)%(refname:short)%(color:reset) - %(contents:subject) %(color:green)(%(committerdate:relative)) [%(authorname)]' --sort=-committerdate"
-git config --global alias.c commit
+git config --global alias.c "commit"
 git config --global alias.cm "commit -m"
 git config --global alias.cam "commit -am"
-
+git config --global alias.m "merge"
 git config --global alias.s "status -s"
-git config --global alias.st status
+git config --global alias.st "status"
 git config --global alias.la "!git config -l | grep alias | cut -c 7-"
 git config --global alias.last 'log -1 HEAD'
-# git config --global alias.unstage 'reset HEAD --'
 
 # configure git proxy to use v2ray proxy for github if necessary
 # git config --global http.https://github.com.proxy http://127.0.0.1:10809
