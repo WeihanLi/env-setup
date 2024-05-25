27fbd3832963de74dae6ef7360665902a77f86b1
Weihan Li <weihanli@outlook.com>
2024-05-25 09:55:33 +0800

    refactoring dotnet-tools

--- windows/dotnet-configure.ps1
+++ windows/dotnet-configure.ps1
@@ -2,7 +2,10 @@ Write-Host "Setting up dotnet for Windows..." -ForegroundColor Green
 Write-Host "------------------------------------" -ForegroundColor Green
 [Environment]::SetEnvironmentVariable("DOTNET_PRINT_TELEMETRY_MESSAGE", "false", "Machine")
 [Environment]::SetEnvironmentVariable("DOTNET_CLI_TELEMETRY_OPTOUT", "1", "Machine")
-dotnet tool update --global dotnet-execute --prerelease
-dotnet tool update --global dotnet-httpie
-dotnet tool update --global dotnet-outdated-tool
-dotnet tool update --global dotnet-ef
+
+$dotnetToolsJson = Get-Content ..\shared\dotnet-tools.json
+$dotnetTools = $dotnetToolsJson | ConvertFrom-Json
+foreach ($tool in $dotnetTools) {
+    Write-Output "dotnet tool update --global $tool"
+    dotnet tool update --global $tool
+}
